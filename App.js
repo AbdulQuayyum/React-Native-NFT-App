6e9bff6521797f9de08d95e3134a28106f2045a6
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-13 22:42:48 +0100

    first commit

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { useFonts } from "expo-font";
 
-import Home from "./screens/Home";
-import Details from "./screens/Details";
+import Home from "./screens/Home"
+import Details from "./screens/Details"
 
 const theme = {
   ...DefaultTheme,
@@ -18,11 +18,11 @@ const Stack = createStackNavigator();
 
 const App = () => {
   const [loaded] = useFonts({
-    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
-    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
-    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
-    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
-    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+    InterBold : require("./assets/fonts/Inter-Bold"),
+    InterSemiBold : require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMediumBold : require("./assets/fonts/Inter-Meddium.ttf"),
+    InterRegularBold : require("./assets/fonts/Inter-Regular.ttf"),
+    InetrLighter : require("./assets/fonts/InetrLight.ttf")
   });
 
   if (!loaded) return null;
