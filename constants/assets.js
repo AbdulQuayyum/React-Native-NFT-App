e9f2963cde759f48a1ef901a8323998434bc7f35
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-15 22:34:06 +0100

    first commit

--- constants/assets.js
+++ constants/assets.js
@@ -5,13 +5,16 @@ import left from "../assets/icons/left.png";
 import search from "../assets/icons/search.png";
 
 import logo from "../assets/images/logo.png";
-import nft01 from "../assets/images/nft01.png";
-import nft02 from "../assets/images/nft02.jpeg";
-import nft03 from "../assets/images/nft03.jpeg";
-import nft04 from "../assets/images/nft04.jpeg";
-import nft05 from "../assets/images/nft05.jpeg";
-import nft06 from "../assets/images/nft06.jpeg";
-import nft07 from "../assets/images/nft07.jpeg";
+import nft07 from "../assets/images/nft.png";
+import nft01 from "../assets/images/nft01.jpg";
+import nft02 from "../assets/images/nft02.jpg";
+import nft03 from "../assets/images/nft03.jpg";
+import nft04 from "../assets/images/nft04.jpg";
+import nft05 from "../assets/images/nft05.jpg";
+import nft06 from "../assets/images/nft06.jpg";
+import nft07 from "../assets/images/nft07.jpg";
+import nft07 from "../assets/images/nft08.jpg";
+import nft07 from "../assets/images/nft09.jpg";
 
 import person01 from "../assets/images/person01.png";
 import person02 from "../assets/images/person02.png";
@@ -26,6 +29,7 @@ export default {
   search,
 
   logo,
+  nft,
   nft01,
   nft02,
   nft03,
@@ -33,6 +37,8 @@ export default {
   nft05,
   nft06,
   nft07,
+  nft08,
+  nft09,
 
   person01,
   person02,
