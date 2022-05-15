ebf7e9fd882d21bbba982f1423a93cfcf58dfc1d
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-15 22:23:57 +0100

    first commit

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { View, SafeAreaView, FlatList, Text } from "react-native";
 
+import { NFTCard, HomeHeader, FocusedStatusBar } from "../components";
+import { COLORS, NFTData } from "../constants";
+
 const Home = () => {
   return (
     <Text>Home</Text>
