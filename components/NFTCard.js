cb996531c4879194a50e36f1f8f2fe7401e0c433
Abdul Quayyum <alaoabdulquayyumm@gmail.com>
2022-05-16 10:51:33 +0100

    first commit

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
-import { View, Image } from "react-native";
+import { View, Image, Text } from "react-native";
 
 import { COLORS, SIZES, SHADOWS, assets } from "../constants";
 import { SubInfo, EthPrice, NFTTitle } from "./SubInfo";
 import { RectButton, CircleButton } from "./Button";
 
 
-const NFTCard = () => {
+const NFTCard = ( { data } ) => {
+    const navigation = useNavigation();
   return (
-    <View>
-     
+    <View style={{
+        backgroundColor: COLORS.white,
+        borderRadius: SIZES.font,
+        marginBottom: SIZES.extraLarge,
+        margin: SIZES.base,
+        ...SHADOWS.dark
+    }}>
+        <View style={{ width: "100%" }}></View>
+       <Text>NFTCard</Text>
     </View>
   )
 }
 
-export default NFTCard
+export default NFTCard 
